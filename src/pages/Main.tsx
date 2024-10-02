ce4e87088cea7623eaaf4192b73c36663a75400c
Parle648 <sashahoumi@gmail.com>
2024-10-02 13:50:31 +0300

    + Create WelcomeWidget

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,8 +1,10 @@
 import ModeList from '../widgets/ModeList/ModeList';
+import WelcomeTitle from '../widgets/WelcomeWidget/WelcomeWidget';
 
 const Main = () => {
   return (
     <div>
+      <WelcomeTitle />
       <ModeList />
     </div>
   );
