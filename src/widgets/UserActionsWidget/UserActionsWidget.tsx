ee90301e40a11e94374dee74a9045d3f8bd390d9
Parle648 <sashahoumi@gmail.com>
2024-10-03 15:29:29 +0300

    * Change UserActionBtns render

--- src/widgets/UserActionsWidget/UserActionsWidget.tsx
+++ src/widgets/UserActionsWidget/UserActionsWidget.tsx
@@ -7,6 +7,8 @@ import { setUserMatchsticks } from '../../features/slices/player-matchsticks';
 
 const UserActions = () => {
   const pile = useSelector((store: any) => store.pile.value);
+  const takeUpTo = useSelector((store: any) => store.takeUpTo.value);
+  const userBtnIndexes: number[] = Array.from(Array(takeUpTo).keys());
   const isBotTake = useSelector((store: any) => store.isBotTake.value);
 
   const getMatchsticks = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -21,18 +23,22 @@ const UserActions = () => {
 
   return (
     <div className={styles.buttonList}>
-      <button className={styles.btn} value={1} onClick={(event) => getMatchsticks(event)}>
-        {' '}
-        +1 Matchstick
-      </button>
-      <button className={styles.btn} value={2} onClick={(event) => getMatchsticks(event)}>
-        {' '}
-        +2 Matchsticks
-      </button>
-      <button className={styles.btn} value={3} onClick={(event) => getMatchsticks(event)}>
-        {' '}
-        +3 Matchsticks
-      </button>
+      {userBtnIndexes.map((index) => {
+        return (
+          <>
+            {pile >= index + 1 && (
+              <button
+                className={styles.btn}
+                value={index + 1}
+                onClick={(event) => getMatchsticks(event)}
+              >
+                {' '}
+                +{index + 1} Matchstick
+              </button>
+            )}
+          </>
+        );
+      })}
     </div>
   );
 };
